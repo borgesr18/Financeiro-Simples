47151e4ba25d870075be096531e8681f6c246945
borgesr18 <borgesr18@yahoo.com.br>
2025-08-15 05:40:06 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,28 @@
 import { NextResponse, NextRequest } from 'next/server'
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next({ request: { headers: req.headers } })
-  // Refresh the session if needed
+
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() { return req.cookies.getAll() },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => res.cookies.set(name, value, options))
+        get(name: string) {
+          return req.cookies.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          res.cookies.set({ name, value, ...options })
+        },
+        remove(name: string, options: CookieOptions) {
+          res.cookies.set({ name, value: '', ...options })
         },
       },
     }
   )
+
+  // Mantém a sessão atualizada
   await supabase.auth.getUser()
   return res
 }
